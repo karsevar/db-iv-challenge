a5183356b3eb940cf907fba3be1b69eee3f98ba9
masonkarsevar <masonkarsevar@gmail.com>
2019-09-12 12:46:52 -0700

    getShoppingList and getInstructions functionality all finished

--- routes/cookBookModel.js
+++ routes/cookBookModel.js
@@ -6,8 +6,18 @@ function getRecipes() {
 
 function getShoppingList(recipe_id) {
     return db('instructions')
-        .where({recipe_id})
+            .join('ingredients', 'instructions.ingredient_id', '=', 'ingredients.id')
+            .join('recipes', 'instructions.recipe_id', '=', 'recipes.id')
+            .where({recipe_id})
+            .select('ingredients.ingredient_name', 'instructions.quantity', 'recipes.recipe_name')
 }
 
-module.exports = {getRecipes, getShoppingList}
+function getInstructions(recipe_id) {
+    return db('instructions') 
+            .join('ingredients', 'instructions.ingredient_id', '=', 'ingredients.id')
+            .where({recipe_id})
+            .select('ingredients.ingredient_name', 'instructions.quantity', 'instructions.step', 'instructions.step_number')            
+}
+
+module.exports = {getRecipes, getShoppingList, getInstructions}
 
--- routes/cookBookRoute.js
+++ routes/cookBookRoute.js
@@ -13,10 +13,20 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id/shoppinglist', (req, res) => {
     cookBookDb.getShoppingList(req.params.id) 
         .then(results => {
             res.status(200).json(results)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
+});
+
+router.get('/:id/instructions', (req, res) => {
+    cookBookDb.getInstructions(req.params.id) 
+        .then(results => {
+            res.status(200).json(results) 
         })
         .catch(error => {
             res.status(500).json(error)
