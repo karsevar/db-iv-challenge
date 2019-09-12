7800088c56d11545993bddd65b59d30f3dffcfcf
masonkarsevar <masonkarsevar@gmail.com>
2019-09-12 12:21:33 -0700

    seed data added to database. WIP: get requests

---
+++ data/seeds/02-ingredients.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex) {
+  // Inserts seed entries
+  return knex('ingredients').insert([
+    {ingredient_name: 'Gala Apple'},
+    {ingredient_name: 'tablespoons butter'},
+    {ingredient_name: 'teaspoon salt'},
+    {ingredient_name: 'eggs'},
+    {ingredient_name: 'butter'},
+    {ingredient_name: 'pounds ribeye'},
+    {ingredient_name: 'cups cocoa'},
+    {ingredient_name: 'onion'},
+    {ingredient_name: 'pounds clams'},
+    {ingredient_name: 'cups sugar'},
+    {ingredient_name: 'cups vegable oil'},
+    {ingredient_name: 'pounds Pork Shoulder'},
+    {ingredient_name: 'cups milk'}
+  ]);
+};
