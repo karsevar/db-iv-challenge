b0a7d1f7d6b89d27e5731a749423eebb4de1551e
masonkarsevar <masonkarsevar@gmail.com>
2019-09-12 08:25:02 -0700

    recipe route connected to base cookBookRoute component

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cookBookRoute = require('./routes/cookBookRoute.js')
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/recipes', cookBookRoute);
 
 server.use('/', (req, res) => {
     res.send(`<h2>Root Route is Printing something!!!</h2>`)
